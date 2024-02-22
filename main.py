3ac04d35142c1d0396b2f1aa28fd87f0df80a83e
ryadhazin0002 <ryad.hazin0002@stud.hkr.se>
2024-02-22 16:44:27 +0100

    make the game depends now on player class

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
 import random
 
+from human_player import HumanPlayer
 
-player_name = input("please enter your name: ")
+player = HumanPlayer(0, input("please enter your name: "), 0)
 round_score = 0
-total_score = 0
+print(f"Welcome {player.name} to PIG Dice Game, Lets Start!!")
 while True:
      diceValue = random.randint(1, 6)
      if diceValue != 1 and round_score < 100:
@@ -12,16 +13,15 @@ while True:
          print (f"Your current score is {round_score}")
          if round_score >= 100:
              print("🎉🎉🎉🎉🎉🎉🎉🎉🎉🎉🎉🎉🎉🎉🎉🎉🎉🎉🎉")
-             print(f"🎉🎉🎉 congratulations you won 🎉🎉🎉")
+             print(f"🎉🎉🎉 congratulations {player.name}, you won 🎉🎉🎉")
              print("🎉🎉🎉🎉🎉🎉🎉🎉🎉🎉🎉🎉🎉🎉🎉🎉🎉🎉🎉")
              break
-         roll_again = input("Roll again or Hold? 'r' or 'h': ").rstrip()
+         roll_again = player.take_action()
          if roll_again == "r":
              continue
          elif roll_again == "h":
-             total_score += round_score
-             round_score = 0
-             print (f"Your total score is {total_score}")
+             player.total_score = round_score
+             print (f"Your total score is {player.total_score}")
              print("**********************************************************************")
              continue
          else:
@@ -29,6 +29,6 @@ while True:
              valid_choice = input("Roll again or Hold? 'r' or 'h': ")
      
      elif diceValue == 1:
-         round_score = 0
+         round_score = player.total_score
          print("You have lost your round score!!!")
          print("**********************************************************************")
