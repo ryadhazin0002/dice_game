13fdbc9007cd21ee7c035c934cc7c6d80867800e
ryadhazin0002 <ryad.hazin0002@stud.hkr.se>
2024-02-23 00:48:48 +0100

    add highscore to HumanPlayer

--- human_player.py
+++ human_player.py
@@ -3,10 +3,12 @@ from player import Player
 class HumanPlayer(Player):
 
     id: str
+    high_scores: list[str]
 
-    def __init__(self, id,name, total_score):
-        super().__init__(name, total_score)
+    def __init__(self, id,name, high_scores):
+        super().__init__(name, 0)
         self.id = id
+        self.high_scores = high_scores
 
     def take_action(self):
         return input("Roll again or Hold? 'r' or 'h': ").rstrip()
