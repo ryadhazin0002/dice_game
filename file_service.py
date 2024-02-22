2c267649e619d496d58b402aeeb3000a875d5d5f
ryadhazin0002 <ryad.hazin0002@stud.hkr.se>
2024-02-23 00:47:21 +0100

    fix player saving data process

--- file_service.py
+++ file_service.py
@@ -10,24 +10,26 @@ class FileService:
     filename: str
 
     def load_players(self) -> list[HumanPlayer]:
-        players: list[HumanPlayer]
+        players: list[HumanPlayer] = []
         with open (self.filename) as file:
             while True:
                 line = file.readline().rstrip("\n")
                 if line == "": break
-                line = line.split(",")
+                line = line.split(":")
                 id = line[0]
                 name = line[1]
-                score = line[2]
-                player = HumanPlayer(id, name, score)
+                high_score = line[2].split(",")
+                if line[2] == '':
+                    high_score = []
+                player = HumanPlayer(id, name, high_score)
                 players.append(player)
-        return player
+            return players
     
     def save_players(self, players: list[HumanPlayer]):
         with open (self.filename, "w") as file:
             for player in players:
-                file.write(f"{player.id},{player.name},{player.total_score}\n")
+                file.write(f"{player.id}:{player.name}:{str.join(',',player.high_scores)}\n")
 
     def add_player(self, player: HumanPlayer):
         with open (self.filename, "a") as file:
-                file.write(f"{player.id},{player.name},{player.total_score}\n")
+                file.write(f"{player.id}:{player.name}:\n")
