0e82036a5c43cc16ec670589f37a5220cd62cb14
zakaria <zakaria.majkouma0013@stud.hkr.se>
2024-02-28 16:57:59 +0100

    Adding easy and hard level to COPlayer

--- src/pig/co_player.py
+++ src/pig/co_player.py
@@ -2,15 +2,26 @@ from player import Player
 import random
 import time
 
-
 class COPlayer(Player):
 
-    def __init__(self, difficulty):
-        super().__init__("🤖", 0)
+    def init(self, difficulty):
+        super().init("🤖", 0)
         self.difficulty = difficulty
 
-    def take_action(self):
-        choice = random.choice(["r", "h"])
+    def take_action(self, round_score):
+        if self.difficulty == "easy" and round_score + self.total_score < 80:
+            choice = random.choice(["r", "h"])
+        elif self.difficulty == "hard" and round_score < 9:
+            choice = "r"
+        elif self.difficulty == "hard" and round_score >= 9:
+            choice = random.choice(["r", "h"])
+        elif self.difficulty == "hard" and round_score + self.total_score >= 90:
+            choice = "r"
+        elif self.difficulty == "hard" and round_score + self.total_score >= 100:
+            choice = "h"
+        # else:
+        #     choice = "h"
+
         if choice == "r":
             time.sleep(1)
             print("Roll")
