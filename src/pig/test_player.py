745feb1f71100f7789c74c873a83de4ab94a1081
mustafa.hkr <mustafa.al-bayati0036@stud.hkr.se>
2024-03-04 19:14:40 +0100

    test player player name and player total score

--- src/pig/test_player.py
+++ src/pig/test_player.py
@@ -1,8 +1,18 @@
 import unittest
 from player import Player
+from abc import ABC
+
+class Player(ABC):
+    class TestPlayer(unittest.TestCase):
+        def setUp(self):
+            self.player = Player("TestPlayer", 0)
+        
+    def test_player_creation(self):
+        self.assertEqual(self.player.name, "TestPlayer")
+        self.assertEqual(self.player.total_score, 0)
+        
 
 
-class TestPlayer(unittest.TestCase):
-    def setUp(self):
-        self.player = Player("TestPlayer", 0)
+if __name__ == '__main__':
+    unittest.main()
     
