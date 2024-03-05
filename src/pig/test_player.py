7bb1a94683a43e19e6376bbe60dad49558c411f7
mustafa.hkr <mustafa.al-bayati0036@stud.hkr.se>
2024-03-05 15:50:29 +0100

    test player second part

--- src/pig/test_player.py
+++ src/pig/test_player.py
@@ -13,6 +13,15 @@ class Player(ABC):
         pass
 
 
+class ConcretePlayer(Player):
+    def take_action(self, current_score: int):
+        if current_score < 50:
+            return "r"
+        else:
+            return "h"
+
+
+
 
 if __name__ == '__main__':
     unittest.main()
