c41c86f992cdc31320299ddf5b21e2658acc7c8f
mustafa.hkr <mustafa.al-bayati0036@stud.hkr.se>
2024-03-06 12:21:14 +0100

    test_file_service second.method

--- src/pig/test_file_service.py
+++ src/pig/test_file_service.py
@@ -11,6 +11,19 @@ class TestFileService(unittest.TestCase):
             f.write("1:Ryad:100,200,120\n")
             f.write("2:Mustafa:150,120\n")
 
+    def test_load_players(self):
+        players = self.file_service.load_players()
+        self.assertEqual(len(players), 2)
+        self.assertIsInstance(players[0], HumanPlayer)
+        self.assertIsInstance(players[1], HumanPlayer)
+        self.assertEqual(players[0].id, "1")
+        self.assertEqual(players[0].name, "Ryad")
+        self.assertEqual(players[0].high_scores, ["100", "200", "120"])
+        self.assertEqual(players[1].id, "2")
+        self.assertEqual(players[1].name, "Mustafa")
+        self.assertEqual(players[1].high_scores, ["150", "120"])
+        self.assertTrue(os.path.exists(self.file_service.filename))
+
 
 
 if __name__ == '__main__':
