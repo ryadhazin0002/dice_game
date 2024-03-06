5724410ea502dc99ac24858ada82859887d6415e
mustafa.hkr <mustafa.al-bayati0036@stud.hkr.se>
2024-03-06 22:54:31 +0100

    test_dice third

--- src/pig/test_dice.py
+++ src/pig/test_dice.py
@@ -57,6 +57,28 @@ class TestDice(unittest.TestCase):
             curses.nocbreak()
             curses.endwin()
 
+    def test_print_to_terminal(self):
+        dice = Dice()
+        expected_output_3 = [
+            "+-------+",
+            "| o     |",
+            "|   o   |",
+            "|     o |",
+            "+-------+",
+        ]
+
+        with patch('sys.stdout', new=StringIO()) as mock_stdout:
+ 
+            dice.print_to_terminal(3)
+
+            actual_output = mock_stdout.getvalue().strip()
+
+            for expected_line in expected_output_3:
+                if expected_line.strip() == "|       |":
+                    continue 
+                self.assertIn(expected_line.strip(), actual_output)
+            
+
 
 if __name__ == '__main__':
     unittest.main()
